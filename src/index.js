cdcede7aceaabd98b85a68de6aa4bf8670cbc55d
Marky <johnmarky.dev@gmail.com>
2023-12-20 09:56:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -30,14 +30,10 @@ app.use((error, req, res, next) => {
 
 const PORT = process.env.PORT || 8080;
 mongoose
-  .connect(
-    process.env.MONGO_URL ||
-      "mongodb+srv://johnmarky:jmn08082006@users.14sswne.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
